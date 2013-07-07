2d5fdb7bfab0c91be13f238b7e6cd8a199b54ff3
njones-fiksu <njones@fiksu.com>
2013-07-06 21:36:17 -0400

    Write to files, R script to read in from files and combine

--- team_pbp.py
+++ team_pbp.py
@@ -2,7 +2,7 @@
 
 import re
 import math
-
+import os
 
 '''
 TODO: 
@@ -28,8 +28,14 @@ class TimeLine:
         self.teams = scores.keys()
         self.scores = [scores[self.teams[0]], scores[self.teams[1]]]
 
+    def get_diff(self, team_with_respect_to):
+        # Get differential with respect to team <team_with_respect_to>
+        index_with_respect_to = 1 if self.teams[1] == team_with_respect_to else 0
+        other_index = (index_with_respect_to + 1) % 2
+        differential = [self.scores[index_with_respect_to][t] - self.scores[other_index][t] for t in range(len(self.times))]
+        return differential
 
-def readAllGames(fileName = 'first5000.txt'):
+def readAllGames(fileName = 'playbyplay20120510040.txt'):
     f = open(fileName, 'r')
     allLines = f.readlines()
     f.close()
@@ -113,8 +119,6 @@ def writeTeamTS(times, scores, teams, outFile = 'oneSeries.csv'):
         out.write(str(t) + ', ' + str(s) + '\n')
     out.close()
 
-        
-
 def convertTime(fileTime):
     '''
     Takes a string from the file\'s TimeRemaining column,
@@ -125,3 +129,40 @@ def convertTime(fileTime):
     hrs, mins, secs = re.search(tRexp, fileTime).groups()
     elapsed = GAME_LENGTH - (int(mins) + int(secs) / 60.0)
     return elapsed
+
+def write_games_to_files(team = 'MIA'):
+    all_games = readAllGames()
+    team_games = all_games[team]
+    # Set up directory for data
+    make_dir_for_team(team)
+
+    for g in team_games:
+        date = g.date
+        tl = g.timeline
+        # Could use sets here instead
+        other_team = tl.teams[0] if tl.teams[1] == team else tl.teams[1]
+        file_name = team + '/' + date + '-' + other_team + '.csv'
+        differential = tl.get_diff(team)
+        write_one_game_to_file(file_name, tl.times, differential)
+
+def make_dir_for_team(team):
+    ''' Make directory to store this team's timeline data '''
+    try:
+        os.mkdir(team)
+    except OSError:
+        print "Directory '%s' already existed, nothing overwritten" % team
+
+def write_one_game_to_file(file_name, times, diffs):
+    ''' Write to <file_name> the time and point differential as a .csv'''
+    try:
+        f = open(file_name, 'w')
+        for t, d in zip(times, diffs):
+            f.write("%s, %i \n" % (t, d))
+        f.close()
+        print 'Wrote %s successfully!' % file_name
+    except:
+        print 'Failed to create file %s!' % file_name
+
+
+
+
--- viz.R
+++ viz.R
@@ -1,5 +1,26 @@
-game <- read.csv('oneSeries.csv', skip = 1, head = T)
 
-maxDiff <- max(abs(game$diff))
+full_df <- data.frame(matrix(nrow = 0, ncol = 3))
+colnames(full_df) <- c('date_opp', 'time', 'diff')
+
+for(f in list.files()) {
+  time_series <- read.csv(f, head = F)
+  # Add opponent info in another column
+  time_series <- cbind(f, time_series)
+  names(time_series) <- c('date_opp', 'time', 'diff')
+  print(head(time_series))
+  full_df <- merge(full_df, time_series, all = T)
+}
+full_df$date_opp <- as.character(full_df$date_opp)
+full_df$time <- as.numeric(full_df$time)
+full_df$diff <- as.integer(full_df$diff)
+
+plot_tls <- ggplot(data = full_df, aes(time, diff, color = date_opp)) +
+  geom_point(pch = '.') + geom_line(alpha = .5)
+plot_tls
+plot_time_series <- function(ts) {
+  maxDiff <- max(abs(ts$diff))
+  
+}
+
 # Looks like BM! Maybe it is, when teams are evenly matched?
 qplot(time, diff, data = game, ylim = c(-maxDiff, maxDiff), geom = 'line')
